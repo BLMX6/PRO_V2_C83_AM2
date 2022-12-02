8447051126a4a739ecdc5f52b678c1cc62c6291c
BLMX6 <119624247+BLMX6@users.noreply.github.com>
2022-12-01 20:12:01 -0600

    Update TabNavigator.js

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -34,8 +34,8 @@ const BottomTabNavigator = () => {
       activeColor={"#ee8249"}
       inactiveColor={"gray"}
     >
-      <Tab.Screen name="Feed" component={Feed} options={{headerShown:false}}/>
-      <Tab.Screen name="Create Story" component={CreateStory} options={{headerShown:false}}/>
+      <Tab.Screen name="Índice" component={Feed} options={{headerShown:false}}/>
+      <Tab.Screen name="Crear historia" component={CreateStory} options={{headerShown:false}}/>
     </Tab.Navigator>
   );
 };
